81615e3f28c82c74adf8bb4715b798ea72a248ca
erbasmelahat <erbasmelahatt@gmail.com>
2024-12-26 02:54:00 +0300

    Added blog section and made responsive adjustments to home and contact pages.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,48 +1,68 @@
-import React from 'react'
-import Images1 from '../images/14.jpg';
+import React from "react";
+import Images1 from "../images/14.jpg";
 
 function Contact() {
   return (
     <div>
-    <div className="hero bg-base-200 min-h-screen "   style={{
+      <div
+        className="hero bg-base-200 min-h-screen "
+        style={{
           backgroundImage: `url(${Images1})`,
-        }}>
-        <div className="hero-overlay bg-opacity-40 "></div>
+        }}
+      >
+        <div className="hero-overlay bg-opacity-30 "></div>
 
-  <div className="hero-content flex-col lg:flex-row-reverse gap-10">
-    <div className="text-center lg:text-left">
-      <h1 className="text-5xl font-bold">Login now!</h1>
-      <p className="py-6">
-        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-        quasi. In deleniti eaque aut repudiandae et a id nisi.
-      </p>
-    </div>
-    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-      <form className="card-body">
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Email</span>
-          </label>
-          <input type="email" placeholder="email" className="input input-bordered" required />
+        <div className="hero-content flex-col lg:flex-row-reverse gap-10">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">Bize Ulaşın!</h1>
+            <p className="py-6 text-slate-300 text-lg ">
+              Her zaman sizinle iletişimde olmak bizim için büyük bir mutluluk!
+              Sorularınızı yanıtlamak, önerilerinizi dinlemek veya
+              siparişleriniz hakkında size yardımcı olmak için buradayız.
+              <br />
+              Eğer bir konuda destek almak isterseniz ya da sadece "Merhaba"
+              demek isterseniz, aşağıdaki iletişim kanallarımızı
+              kullanabilirsiniz.
+            </p>
+          </div>
+          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+            <form className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                  required
+                />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Login</button>
+              </div>
+            </form>
+          </div>
         </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Password</span>
-          </label>
-          <input type="password" placeholder="password" className="input input-bordered" required />
-          <label className="label">
-            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-          </label>
-        </div>
-        <div className="form-control mt-6">
-          <button className="btn btn-primary">Login</button>
-        </div>
-      </form>
+      </div>
     </div>
-  </div>
-</div>
-    </div>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
-import Images1 from "../images/2.jpg";
+import Images1 from "../images/6.jpg";
+import Images2 from "../images/14.jpg";
 
 function Home() {
   return (
@@ -10,24 +11,24 @@ function Home() {
           backgroundImage: `url(${Images1})`,
         }}
       >
-        <div className="hero-overlay bg-opacity-40 "></div>
+        <div className="hero-overlay bg-opacity-20 "></div>
         <div className="hero-content text-neutral-content">
           <div className="max-w-3xl items-end justify-end">
-            <h1 className="mb-5 text-5xl font-bold">
+            <h1 className="mb-5 text-5xl font-bold text-slate-300 ">
               En Lezzetli Burger Deneyimi Seni Bekliyor!
             </h1>
-            <p className="mb-5 text-lg">
+            <p className="mb-5 text-lg text-slate-300">
               Burger aşıklarına özel, taptaze malzemelerle hazırladığımız efsane
               burgerlerimizle tanışmaya hazır mısınız? Her lokmada gerçek
               lezzeti hissedin! Klasikten gurme seçeneklere, damak tadınıza
               uygun zengin bir menüyle karşınızdayız. Şimdi sipariş verin, enfes
               lezzetlerimizi denemenin tam zamanı!
             </p>
-            <button className="btn ">Lezzeti Keşfet!</button>
+            <button className="btn text-gray-300">Lezzeti Keşfet!</button>
           </div>
         </div>
       </div>
-      <div className="flex w-full justify-center items-center p-10">
+      {/* <div className="flex w-full justify-center items-center p-10">
         <div className="flex max-w-full justify-center  gap-5">
           <div className="chat chat-start">
             <div className="chat-bubble">
@@ -38,6 +39,77 @@ function Home() {
             <div className="chat-bubble">
               Siparişleriniz genellikle 15-20 dakika içinde hazırlanır. Teslimat
               süresi ise bulunduğunuz bölgeye bağlıdır.
+            </div>
+          </div>
+        </div>
+      </div> */}
+
+      <div className="flex flex-col lg:flex-row flex-wrap gap-10 p-10 justify-center items-center">
+        <div className="card bg-base-100 shadow-xl w-full lg:w-1/4">
+          <figure>
+            <img
+              src={Images2}
+              className="h-48 w-full object-cover"
+              alt="Album"
+            />
+          </figure>
+          <div className="card-body bg-base-300">
+            <h2 className="card-title">
+              Burgerin Tarihçesi: İlk Hamburger
+            </h2>
+            <p>
+              Hamburger, 19. yüzyılda Almanya’nın Hamburg şehrinden ilham alarak
+              ortaya çıktı. Amerika’ya göç eden Almanlar sayesinde popülerleşti
+              ve 1904 St. Louis Dünya Fuarı’nda ekmek arasında servis edilerek
+              bugünkü halini aldı.
+            </p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Listen</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="card bg-base-100 shadow-xl w-full lg:w-1/4">
+          <figure>
+            <img
+              src={Images2}
+              className="h-48 w-full object-cover"
+              alt="Album"
+            />
+          </figure>
+          <div className="card-body bg-base-300">
+            <h2 className="card-title">Mükemmel Burger Nasıl Hazırlanır?</h2>
+            <p>
+              Kaliteli malzemeler mükemmel bir burgerin anahtarıdır. Sulu bir
+              köfte, taze sebzeler, eriyen peynir ve özel soslar, hafif
+              kızartılmış ekmekle birleşerek ideal lezzeti oluşturur.
+              Malzemeleri doğru oranlarda bir araya getirmek başarının sırrıdır.
+            </p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Listen</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="card bg-base-100 shadow-xl w-full lg:w-1/4">
+          <figure>
+            <img
+              src={Images2}
+              className="h-48 w-full object-cover"
+              alt="Album"
+            />
+          </figure>
+          <div className="card-body bg-base-300">
+            <h2 className="card-title">
+              Vejetaryen ve Vegan Burger Seçenekleri Artıyor!
+            </h2>
+            <p>
+              Vejetaryen ve vegan burgerler, bitki bazlı köfteler, peynirler ve
+              soslarla hem lezzetli hem de sağlıklı bir alternatif sunuyor.
+              Artık herkes için yaratıcı ve sürdürülebilir seçenekler mevcut!
+            </p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Listen</button>
             </div>
           </div>
         </div>
